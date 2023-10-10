25581290f711382670ca83aee1c0db95922cabe5
Fyhne <kylehvdbnl@gmail.com>
2023-10-10 15:01:43 -0700

    Database added, entry added, search feature added

--- main.py
+++ main.py
@@ -1,9 +1,64 @@
+
 from tkinter import *
 from PIL import ImageTk, Image
 import sqlite3
 
 
 root = Tk()
+
+
+
+# Delete Entry Widget and Label
+delete_entry = Entry(root, width=30)
+delete_entry.grid(row=8, column=1, padx=20)
+delete_label = Label(root, text="Delete by Item Name:")
+delete_label.grid(row=8, column=0, padx=20)
+
+# Delete Button and Function
+def delete_record():
+    conn = sqlite3.connect('inventory.db')
+    c = conn.cursor()
+
+    # Get the item name to be deleted from the entry widget
+    delete_term = delete_entry.get()
+
+    # Delete records that match the specified item name
+    c.execute("DELETE FROM storage WHERE item_name=?", (delete_term,))
+    conn.commit()
+    conn.close()
+
+delete_btn = Button(root, text="Delete Record", command=delete_record)
+delete_btn.grid(row=8, column=2)
+
+search_entry = Entry(root, width=30)
+search_entry.grid(row=6, column=1, padx=20)
+search_label = Label(root, text="Search by Item Name:")
+search_label.grid(row=6, column=0, padx=20)
+
+# Search Button and Function
+def search():
+    conn = sqlite3.connect('inventory.db')
+    c = conn.cursor()
+
+    # Get the search term from the entry widget
+    search_term = search_entry.get()
+
+    # Query the database for records matching the search term
+    c.execute("SELECT * FROM storage WHERE item_name LIKE ?", ('%' + search_term + '%',))
+    records = c.fetchall()
+
+    # Display the search results
+    search_results = ''
+    for record in records:
+        search_results += f"Item Name: {record[0]}, Row: {record[1]}, Column: {record[2]}\n"
+
+    result_label = Label(root, text=search_results)
+    result_label.grid(row=7, column=0, columnspan=2)
+
+    conn.close()
+
+search_btn = Button(root, text="Search", command=search)
+search_btn.grid(row=6, column=2)
 
 #creating a databsae
 #conn = sqlite3.connect('inventory.db')
@@ -13,6 +68,8 @@ root = Tk()
 #commit changes
 #conn.commit()
 
+#add part number
+#add items add 
 
 #entry widgets
 tentry = Entry(root, width = 30)
@@ -22,15 +79,13 @@ rentry.grid(row = 1, column= 1, padx = 20)
 hentry = Entry(root, width = 30)
 hentry.grid(row = 2, column = 1, padx = 20)
 
-
 #widget labels
 tlabel = Label(root, text = "Item entry: ")
 tlabel.grid(row = 0, column=0, padx= 20)
 rlabel = Label(root, text = "Row entry: ")
 rlabel.grid(row = 1, column = 0, padx = 20)
-hlabel = Label(root, text = "Height entry: ")
+hlabel = Label(root, text = "Column entry: ")
 hlabel.grid(row = 2, column = 0, padx = 20)
-
 
 #submit button and function
 def submit():
@@ -71,7 +126,7 @@ def output():
     #run thru results
     print_records = ''
 
-    for record in record:
+    for record in record[0]:
         print_records += str(record) + '\n'
 
     querylabel = Label(root, text = print_records)
@@ -81,18 +136,17 @@ def output():
     return
 
 sub_btn = Button(root, text = "Add records to database", command = submit)
-sub_btn.grid(row = 3, column= 1, columnspan= 2, pady = 10, padx= 10, ipadx= 20)
-out_btn = Button(root, text = "Show Records", command = output)
-out_btn.grid(row = 4, column= 1, columnspan= 2, pady = 10, padx= 10, ipadx= 20)
+sub_btn.grid(row = 10, column= 1, columnspan= 2, pady = 10, padx= 10, ipadx= 20)
+
 #creatingsub_btn.grid(row = 3, column= 1, columnspan= 2, pady = 10, padx= 10, ipadx= 20) table
 
 #c.execute('''
-#          CREATE TABLE storage (
-#          item_name text,
-#          row integer,
-#          height integer
-#          )
-#          ''')
+ #         CREATE TABLE storage (
+  #        item_name text,
+   #       row integer,
+    #      height integer
+     #     )
+      #    ''')
 
 
 #close connection
